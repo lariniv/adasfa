f0513e558ad641e7222e1b9850180401819e1608
Ivan Larin <n.larivanko@gmail.com>
2024-07-11 12:25:09 +0300

    fix: move footer to main layout

--- app/(pages)/(main)/(home)/page.tsx
+++ app/(pages)/(main)/(home)/page.tsx
@@ -1,9 +1,10 @@
-import HeroSection from "./components/HeroSection";
-import ForVendorsSection from "./components/ForVendorsSection";
-import ForClientsSection from "./components/ForClientsSection";
-import AboutUsSection from "./components/AboutUsSection";
-import TeamSection from "./components/TeamSection";
-import OurPartners from "./components/OurPartnersSection";
+import HeroSection from './components/HeroSection';
+import ForVendorsSection from './components/ForVendorsSection';
+import ForClientsSection from './components/ForClientsSection';
+import AboutUsSection from './components/AboutUsSection';
+import TeamSection from './components/TeamSection';
+import OurPartners from './components/OurPartnersSection';
+import Footer from '@/app/components/Footer';
 
 export default function Home() {
   return (
@@ -19,6 +20,8 @@ export default function Home() {
       <OurPartners />
 
       <TeamSection />
+
+      <Footer />
     </>
   );
 }
